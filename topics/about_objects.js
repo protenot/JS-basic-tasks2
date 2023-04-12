34b48ae64944e53e82b2aef340a0cf2889c639df
Olga_Belaya <protenot@yandex.ru>
2023-04-12 17:25:16 +0300

    half done

--- topics/about_objects.js
+++ topics/about_objects.js
@@ -3,7 +3,7 @@ module("About Objects (topics/about_objects.js)");
 
 test("object type", function() {
     var empty_object = {};
-    equal("object", typeof(empty_object), 'what is the type of an object?');
+    equal('object', typeof(empty_object), 'what is the type of an object?');
 });
 
 test("object literal notation", function() {
@@ -36,7 +36,7 @@ test("adding functions", function() {
         name: "Amory Blaine",
         age: 102,
         toString: function() {
-            return ___;  // HINT: use the 'this' keyword to refer to the person object.
+            return ('I '+ this.name+' am '+this.age +' years old.' ) ;  // HINT: use the 'this' keyword to refer to the person object.
         }
     };
     equal("I Amory Blaine am 102 years old.", person.toString(), "what should the toString function be?");
